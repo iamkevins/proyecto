f47fa184c1792a6d8b45155b10a0bc9b7639eb98
iamkevins <41312986+iamkevins@users.noreply.github.com>
2018-12-22 23:41:57 -0300

    la embarre

--- index.php
+++ index.php
@@ -1,4 +1,92 @@
-<?php 
-include_once("home.html"); 
-include_once("style.css");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>
+		
+	</h1>
+	
+	<header class="parallax">
+		<div class="divtexto">
+			<h2 >
+				Nuestro amor es hermoso
+			</h2>
+		</div>
+	</header>
+	<section class="center espacio">
+			<h2>
+				Mi vida
+			</h2>
+			<p>
+				<br>
+				Desde que estamos juntos hemos pasdo por altos y bajos, por aledrias y penas...
+				<br>
+				Hemos pasado por mucho y aún estamos juntos y con ganas de amarnos cada día.
+				<br>
+				Eso es lo que mas metiva a seguir adelante con nuetro amor y con nustros proyectos.
+				<br>
+				De tan solo pensar que tendremos un bebe me da tanta alegria y emocion.
+				<br><br>
+				Me hacer muy feliz.
+				<br><br>
+				TE AMO 
+			</p>
+	</section>
+	<section class="parallax2"> 
+		<h2>
+			<br><br><br>
+			Eres Mi Reina Hermosa
+		</h2>
+	</section>
+<section class="center2 espacio2">
+			<h2>
+				Una vez te dedique este poema y aun siento lo mismo
+			</h2>
+			<p>
+			<br>
+No te amo como si fueras rosa de sal, topacio<br> 
+o flecha de claveles que propagan el fuego:<br> 
+te amo como se aman ciertas cosas oscuras,<br> 
+secretamente, entre la sombra y el alma.
+<br><br>
+
+Te amo como la planta que no florece y lleva<br> 
+dentro de sí, escondida, la luz de aquellas flores, <br>
+y gracias a tu amor vive oscuro en mi cuerpo<br> 
+el apretado aroma que ascendió de la tierra.
+<br><br>
+
+Te amo sin saber cómo, ni cuándo, ni de dónde,<br> 
+te amo directamente sin problemas ni orgullo:<br> 
+así te amo porque no sé amar de otra manera, 
+<br><br>
+
+sino así de este modo en que no soy ni eres,<br> 
+tan cerca que tu mano sobre mi pecho es mía,<br> 
+tan cerca que se cierran tus ojos con mi sueño.<br><br>
+			</p>
+	</section>
+	<section class="parallax3"> 
+		<h2>
+			<br><br><br>
+			TE AMO MI CIELO
+		</h2>
+	</section>
+
+	<section class="center3 espacio3">
+			<h2>
+				Te amooooooo...
+			</h2>
+			
+			<p>
+				Posdata:
+				<br>
+				faltan mas fotos juntos
+			</p>
+	</section>	
+</body>
+</html>
